6fa1e51b225870850e48200d18e8c0a9f8ffe365
Ahaag <aaronm.haag@gmail.com>
2023-06-13 14:13:05 -0400

    progress

--- src/app/components/colorPalette.tsx
+++ src/app/components/colorPalette.tsx
@@ -1,28 +1,43 @@
-
-
-
 export interface Palette {
-		shades:{
-		shade50: string;
-		shade100: string;
-		shade200: string;
-		shade300: string;
-		shade400: string;
-		shade500: string;
-		shade600: string;
-		shade700: string;
-		shade800: string;
-		shade900: string;
-		shade950: string;}
+  shades: {
+    shade50: string;
+    shade100: string;
+    shade200: string;
+    shade300: string;
+    shade400: string;
+    shade500: string;
+    shade600: string;
+    shade700: string;
+    shade800: string;
+    shade900: string;
+    shade950: string;
+  };
 }
 
+export default function ColorPalette({ shades }: Palette) {
+  const swatches = [];
 
+  for (const [key, value] of Object.entries(shades)) {
+    const swatchClass = `h-16 w-96 flex justify-between items-center xl:flex-col xl:h-72 hover:drop-shadow-xl `;
+    swatches.push(
+      <div className={swatchClass} style={{ backgroundColor: value }}>
+        <div className="mx-3 rounded-xl bg-[hsla(217,100%,0%,0.2)] xl:my-3">
+          <p className="mx-3 w-10 p-1  text-center font-stencil text-white ">
+            {key.replace(/\D/g, "")}
+          </p>
+        </div>
+        <div className="mx-3 rounded-xl bg-[hsla(217,100%,0%,0.2)] xl:my-3">
+          <p className="mx-3 w-16 p-1 text-center font-stencil text-white">
+            {value}
+          </p>
+        </div>
+      </div>
+    );
+  }
 
-export default function ColorPalette( {shades}:Palette){
-
-	return (
-		<div>
-			{shades.shade50}
-		</div>
-	)
+  return (
+    <div className="m-5 my-4  flex flex-col items-center rounded-xl xl:flex-row">
+      {swatches}
+    </div>
+  );
 }
--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,40 +1,34 @@
-import Image from 'next/image'
-import full_logo from '../../assets/Full-Logo.png'
-
+import Image from "next/image";
+import full_logo from "../../assets/Full-Logo.png";
+import Link from "next/link";
 
 function Header() {
-	return (
-		<header className='header sticky top-0 md:flex items-center justify-between px-8 py-02 bg-jcb-950 w-screen'>
-			<div id='header_logo' className=''>
-				<Image
-					src={full_logo}
-					width={250}
-					height={80}
-					alt='Logo'
-				/>
-			</div>
-			<div>
-
-			</div>
-			<nav className="nav font-semibold text-xl font-light">
-				<ul className="flex items-center">
-					<li className="p-4 text-sm md:text-base text-jcb-50 border-b-2 border-jcb-400 border-opacity-0 hover:border-opacity-100 hover:text-jcb-200 duration-200 cursor-pointer active">
-						<a href="">Home</a>
-					</li>
-					<li className="p-4 text-sm md:text-base text-jcb-50 border-b-2 border-jcb-400 border-opacity-0 hover:border-opacity-100 hover:text-jcb-200 duration-200 cursor-pointer">
-						<a href="">FAQ</a>
-					</li>
-					<li className="p-4 text-sm md:text-base text-jcb-50 border-b-2 border-jcb-400 border-opacity-0 hover:border-opacity-100 hover:text-jcb-200 duration-200 cursor-pointer">
-						<a href="">Support</a>
-					</li>
-					<li className="p-4 text-sm md:text-base text-jcb-50 border-b-2 border-jcb-400 border-opacity-0 hover:border-opacity-100 hover:text-jcb-200 duration-200 cursor-pointer">
-						<a href="">Contribute</a>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	)
-
+  return (
+    <header className="header py-02 sticky top-0 w-screen items-center justify-between bg-jcb-950 px-8 md:flex">
+      <Link href="/">
+        <div id="header_logo" className="">
+          <Image src={full_logo} width={250} height={80} alt="Logo" />
+        </div>
+      </Link>
+      <div></div>
+      <nav className="nav text-xl font-light">
+        <ul className="flex items-center">
+          <li className="active cursor-pointer border-b-2 border-jcb-400 border-opacity-0 p-4 text-sm text-jcb-50 duration-200 hover:border-opacity-100 hover:font-semibold hover:text-jcb-200 md:text-base">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="cursor-pointer border-b-2 border-jcb-400 border-opacity-0 p-4 text-sm text-jcb-50 duration-200 hover:border-opacity-100 hover:font-semibold hover:text-jcb-200 md:text-base">
+            <Link href="/faq">FAQ</Link>
+          </li>
+          <li className="cursor-pointer border-b-2 border-jcb-400 border-opacity-0 p-4 text-sm text-jcb-50 duration-200 hover:border-opacity-100 hover:font-semibold hover:text-jcb-200 md:text-base">
+            <Link href="/support">Support</Link>
+          </li>
+          <li className="cursor-pointer border-b-2 border-jcb-400 border-opacity-0 p-4 text-sm text-jcb-50 duration-200 hover:border-opacity-100 hover:font-semibold hover:text-jcb-200 md:text-base">
+            <Link href="/contribute">Contribute</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
